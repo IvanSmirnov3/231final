36aac2a1683e9678981b84772dde2f180f82b319
IvanSmirnov3 <smirnovcr16@gmail.com>
2025-06-12 16:02:13 +0300

    изм

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.User;
 import web.service.UserService;
@@ -59,7 +58,7 @@ public class UsersController {
         return "redirect:/";
     }
 
-    @PostMapping(value = "/deleteUser")
+    @GetMapping(value = "/deleteUser")
     public String deleteUser(@RequestParam("id") long id) {
         userService.deleteByIdUser(id);
         return "redirect:/";
